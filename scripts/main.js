1d5f423926e500926fadc4a590d313a8a529686c
nate <nathan.j.brenner@gmail.com>
2015-04-02 13:11:26 -0700

    current status, been working on identifying the interseciton of two arrays

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+//global variables
+var badges = ["Beginning HTML and CSS", "HTML First", "CSS: Cascading Style Sheets", "Getting Started with Git", "Working with Remote Repositories", "Basic Layout", "Creating HTML Content", "Adding Pages to a Website", "Branches", "Merging", "Workflows"]
+
+
 // list of students enrolled in class
 
 var classList = function() {
@@ -19,15 +23,74 @@ var badgeList = function() {
 	}
 	$("#tableHead").html(output_html);
 }
-badgeList();
-classList();
+// badgeList();
+// classList();
+
+var treehouseBadges = [];
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+	if(xhr.readyState === 4) {
+		var students = JSON.parse(xhr.responseText);
+		for(var i = 0; i < badges.length; i += 1) {
+			console.log(badges[i]);
+		}
+		for(var i = 0; i < students.badges.length; i += 1) {
+			var badgesCompleted = [students.badges[i].name];
+			console.log(badgesCompleted);
+		}
+		var intersection = _.intersection(badges, badgesCompleted);
+		console.log(intersection);
+
+			// for(var i=0; i<badges.length; i +=1) {
+			// 	if (badgesCompleted.indexOf(badges[i])) {
+			// 		console.log("Nate is awsome");
+			// 	} 
+			// }
+			// console.log(badgesCompleted.indexOf("Newbie"));
+	}
+}
+		// for(var i = 0; i<students.badges.length; i++) {
+		// 	document.write(students.badges[i].name);
+		// }
+		// console.log(students);
+		// console.log(students.badges[0].name
+
+		
+	// 		if (["Blake Johnson"]["badges"]["id"] === "912") {
+	// 			statusHTML += '<td class="yes">';
+	// 		} else {
+	// 			statusHTML += '<td class="no">';
+	// 		}
+	// 		statusHTML += '</td>';
+	// 	// }
+	// statusHTML += '</tr>'
+
+xhr.open('GET', 'http://teamtreehouse.com/katiedyer.json');
+xhr.send();
 
 
 
 
 
-// jquery method//
-// $.getJSON( address, function( data ) {
+
+
+
+
+// $.ajax({
+// 	url: "http://teamtreehouse.com/nathanbrenner.json",
+// 	type:'get',
+// 	dataType: "json",
+// 	success: function(data) {
+//         for(var i = 0; k < badges.length; i += 1) {
+//             console.log(badges);
+//         }
+//     }
+// })
+
+
+
+// $.getJSON( "http://teamtreehouse.com/nathanbrenner.json", function( "nathanbrenner", "nathanbrenner.badges" ) {
 //   var items = [];
 //   $.each( data, function( key, val ) {
 //     items.push( "<li id='" + key + "'>" + val + "</li>" );
@@ -38,6 +101,8 @@ classList();
 //     html: items.join( "" )
 //   }).appendTo( "body" );
 // });
+
+
 
 
 
