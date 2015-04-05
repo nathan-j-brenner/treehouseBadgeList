501731b88680c57415602b9510cfa90d42cc0373
nate <nathan.j.brenner@gmail.com>
2015-04-05 10:25:24 -0700

    cleaned up code in main.js and attempted for loop of the json parse code.  Set in comment where error occurs so I can ask for help

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 //global variables
 var badges = ["Student Name", "Treehouse Username", "Newbie", "Beginning HTML and CSS", "HTML First", "CSS: Cascading Style Sheets", "Why Version Control Matters", "Creating HTML Content", "Adding Pages to a Website", "Getting Started with Git", "Working with Remote Repositories", "Getting Started with CSS", "Base Selectors", "Understanding Values and Units", "Basic Layout", "Float Layout", "Customizing Colors and Fonts", "Styling Web Pages and Navigation", "Responsive Web Design and Testing", "Sharing a Website", "Debugging html and css problems", "Branches", "Merging", "Workflows", "The Box Model", "Designing with the Latest Features", "Fundamental Concepts", "Text, Fonts, Lists", "Backgrounds and Borders", "CSS Gradients", "Chrome DevTools Basics", "Media Basics", "Introducing JavaScript", "Storing and Tracking information with Variables", "Working with Numbers", "Making Decisions with Conditional Statements", "Creating Reusable Code with Functions", "Simplify Repetitive Tasks with Loops", "Tracking Multiple Items with Arrays", "Tracking Data Using Ojects", "Introduction to jQuery", "Introduction to Front-End Frameworks", "Prototyping with Bootstrap"];
-console.log(badges.length);
 var interstion;
 var treehouseURL = 'https://teamtreehouse.com/';
 var student;
@@ -20,15 +19,11 @@ var students = [
 	{name:"Tim Hanson", treehouse:"timhanson", badgesCompleted:[], badgesIntersected:[]},
 	{name:"Traci Reed", treehouse:"tracireed", badgesCompleted:[], badgesIntersected:[]},
 ];
-
 //student list in table
 var classList = function() {
 	var output_html;
 	for (var i=0; i<students.length; i++) {
-		console.log(i);
-		console.log(students[i].name);
-		document.write("<p>" +students[i].name + "</p>");
-		// output_html +="<tr><th>" + students[i].name + "</th></tr>";
+		output_html +="<tr><th>" + students[i].name + "</th></tr>";
 	}
 	$("#badgeTable").html(output_html);
 };
@@ -43,35 +38,41 @@ var badgeList = function() {
 // badgeList();
 // classList();
 
-
-//list Blake's treehouse username
-// console.log(students[0].treehouse);
-//test run for one student THIS CODE WORKS
-
+//This code will find the common badges between what one student has done and what the instructor wants the student to do
 console.log(students[12].name)
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
 	if(xhr.readyState === 4) {
 		var student = JSON.parse(xhr.responseText);
-		// //list Blake's json data
-		// console.log(student);
-		// //display the number of badges blake has completed
-		// console.log(student.badges.length);
-		// // display one badge name
-		// console.log(student.badges[0].name);
-		// // display all of Blake's badges
 		for(var i = 0; i<student.badges.length; i++){
-			console.log(student.badges[i].name);
-			//store student.badges[i].name into students.
 			students[12].badgesCompleted.push(student.badges[i].name);
 		}
 		//display common elements of badges and students badges
 		var intersection = _.intersection(students[12].badgesCompleted, badges);
-		// console.log(intersection);
 		students[12].badgesIntersected.push(intersection);
 		console.log(students[12].badgesIntersected[0]);
 	}
 }
-xhr.open('GET', treehouseURL + students[12].treehouse + '.json');
-xhr.send();
+// xhr.open('GET', treehouseURL + students[12].treehouse + '.json');
+// xhr.send();
 
+
+//Loop for the same function that starts on line 43.  This should go through each student, pull their json file, and list their intersected badges
+for (var i=0; i<students.length; i++) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if(xhr.readyState === 4) {
+			var student = JSON.parse(xhr.responseText);
+			for(var i = 0; i<student.badges.length; i++){
+				// Uncaught TypeError: Cannot read property 'badgesCompleted' of undefined
+				students[i].badgesCompleted.push(student.badges[i].name);
+			}
+			//display common elements of badges and students badges
+			var intersection = _.intersection(students[i].badgesCompleted, badges);
+			students[i].badgesIntersected.push(intersection);
+			console.log(students[i].badgesIntersected[0]);
+		}
+	}
+	// xhr.open('GET', treehouseURL + students[i].treehouse + '.json');
+	// xhr.send();
+}
